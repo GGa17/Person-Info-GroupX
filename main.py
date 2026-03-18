f726ada82a2e6789de47d81a35b97adb73224880
EarlJansenYurong <ejsyurong@cmc.pshs.edu.ph>
2026-03-18 10:22:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 name = input("Name? ")
 print("Hello " + name)
+name = input("Name? ")
+print("Hello " + name)
 age = input("Age? ")
 addr = input("City? ")
 print(name + " lives in " + addr)
 year = 2026 - int(age)
 print("Born in: " + str(year))
-
 pyear = 2026 - int(age)
-
 print("Born in: " + str(year))
